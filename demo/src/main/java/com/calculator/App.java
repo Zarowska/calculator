3e116a631f4dcb4dd5a37a5de24222201d34c5e1
Zarowska <oksana.zarowska@accenture.com>
2021-08-31 10:58:26 +0200

    add class Parser

--- demo/src/main/java/com/calculator/App.java
+++ demo/src/main/java/com/calculator/App.java
@@ -10,7 +10,10 @@ public class App {
 		//String expression = "1+0*2-10*2/5+2";//ok
 		//String expression = "1*2+6/2-8/4+2-1";//ok
 		String expression = Reader.read();
-		MultiExpression e = new MultiExpression(expression);
+		Parser parser = new Parser();
+		Expression parsedExpression = parser.splitStringByOperation(expression, "+");
+		MultiExpression e = new MultiExpression(parsedExpression);
+		
 		double r = e.calculate();
 		System.out.println(r);
 	}
--- demo/src/main/java/com/calculator/MultiExpression.java
+++ demo/src/main/java/com/calculator/MultiExpression.java
@@ -4,68 +4,20 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MultiExpression implements Expression {
-
+// 1+0*2-10*2/5
 	private Expression expression;
 
-	public MultiExpression(String expression) {
-		this.expression = splitStringByOperation(expression, "+");
+	public MultiExpression(Expression parsedExpression) {
+		this.expression = parsedExpression;
+		
 	};
 
-	protected Expression splitStringByOperation(String expression, String operation) {
-		Pattern compiledPattern = Pattern.compile("\\" + operation);
-		Matcher matcher = compiledPattern.matcher(expression);
-		if (matcher.find()) {
-			String leftPart = expression.substring(0, matcher.start());
-			String rightPart = expression.substring(matcher.end(), expression.length());
-			MultiExpression left = new MultiExpression(leftPart);
-			MultiExpression right = new MultiExpression(rightPart);
-			return new SimpleExpression(operation, left, right);
-		}
+	
 
-		Pattern compiledPatternPlus = Pattern.compile("\\" + "+");
-		Matcher matcherPlus = compiledPatternPlus.matcher(expression);
-		if (matcherPlus.find()) {
-			String leftPart = expression.substring(0, matcherPlus.start());
-			String rightPart = expression.substring(matcherPlus.end(), expression.length());
-			MultiExpression left = new MultiExpression(leftPart);
-			MultiExpression right = new MultiExpression(rightPart);
-			return new SimpleExpression("+", left, right);
-		}
-		;
-
-		Pattern compiledPatternMinus = Pattern.compile("\\" + "-");
-		Matcher matcherMinus = compiledPatternMinus.matcher(expression);
-		if (matcherMinus.find()) {
-			String leftPart = expression.substring(0, matcherMinus.start());
-			String rightPart = expression.substring(matcherMinus.end(), expression.length());
-			MultiExpression left = new MultiExpression(leftPart);
-			MultiExpression right = new MultiExpression(rightPart);
-			return new SimpleExpression("-", left, right);
-		}
-		;
-
-		Pattern compiledPatternMultiplicationSign = Pattern.compile("\\" + "*");
-		Matcher matcherMultiplicationSign = compiledPatternMultiplicationSign.matcher(expression);
-		if (matcherMultiplicationSign.find()) {
-			String leftPart = expression.substring(0, matcherMultiplicationSign.start());
-			String rightPart = expression.substring(matcherMultiplicationSign.end(), expression.length());
-			MultiExpression left = new MultiExpression(leftPart);
-			MultiExpression right = new MultiExpression(rightPart);
-			return new SimpleExpression("*", left, right);
-		}
-
-		Pattern compiledPatternDivisionSign = Pattern.compile("\\" + "/");
-		Matcher matcherDivisionSign = compiledPatternDivisionSign.matcher(expression);
-		if (matcherDivisionSign.find()) {
-			String leftPart = expression.substring(0, matcherDivisionSign.start());
-			String rightPart = expression.substring(matcherDivisionSign.end(), expression.length());
-			MultiExpression left = new MultiExpression(leftPart);
-			MultiExpression right = new MultiExpression(rightPart);
-			return new SimpleExpression("/", left, right);
-		}
-
-		return new NumberExpression(expression);
+	public MultiExpression(String leftPart) {
 	}
+
+
 
 	public double calculate() {
 		return expression.calculate();
